45294f12dc61ff227ebd5baa09589a543d45b607
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-12 16:11:46 -0500

    Added Node Copnfig

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/derekjtong/paxos/utils"
+)
 
 func main() {
 	fmt.Println("Hello")
+	node1IP := utils.NodeConfigs[0].IPAddress
+	node1Port := utils.NodeConfigs[0].Port
+	fmt.Printf("%s:%d\n", node1IP, node1Port)
 }
--- utils/config.go
+++ utils/config.go
@@ -1,3 +1,15 @@
 package utils
 
 // Project configs
+
+type NodeConfig struct {
+	NodeID    int
+	IPAddress string
+	Port      int
+}
+
+var NodeConfigs = []NodeConfig{
+	{NodeID: 1, IPAddress: "127.0.0.1", Port: 65432},
+	{NodeID: 2, IPAddress: "127.0.0.1", Port: 65431},
+	{NodeID: 3, IPAddress: "127.0.0.1", Port: 65430},
+}
