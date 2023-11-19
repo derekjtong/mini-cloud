30c136d32b1c0ad1fe985cdecd726170453e64dc
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-18 21:43:33 -0500

    Remamed rpc/server.go

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,4 +1,4 @@
-// rpc/server.go
+// rpc/rpc.go
 
 package rpc
 
@@ -25,6 +25,7 @@ type PingResponse struct {
 }
 
 func (s *RPCServer) Ping(req *PingRequest, res *PingResponse) error {
+	fmt.Printf("[Node %d]: Pinged\n", s.NodeID)
 	res.Message = "Pong from node " + strconv.Itoa(s.NodeID)
 	return nil
 }
@@ -43,18 +44,18 @@ func (s *RPCServer) Start() {
 
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
-		fmt.Printf("[Node%d]: Error starting RPC server on %s: %v\n", s.NodeID, addr, err)
+		fmt.Printf("[Node %d]: Error starting RPC server on %s: %v\n", s.NodeID, addr, err)
 		return
 	}
 
 	defer listener.Close()
 
-	fmt.Printf("[Node%d]: RPC server started on %s\n", s.NodeID, addr)
+	fmt.Printf("[Node %d]: RPC server started on %s\n", s.NodeID, addr)
 
 	rpcServer := rpc.NewServer()
 	err = rpcServer.Register(s)
 	if err != nil {
-		fmt.Printf("[Node%d]: Error registering RPC server: %v\n", s.NodeID, err)
+		fmt.Printf("[Node %d]: Error registering RPC server: %v\n", s.NodeID, err)
 		return
 	}
 
