b3a1ebb2973872b1d351bb5221e3099db0c373c4
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-18 21:56:14 -0500

    Removed hardcoded ports

--- utils/config.go
+++ utils/config.go
@@ -2,14 +2,5 @@ package utils
 
 // Project configs
 
-type NodeConfig struct {
-	NodeID    int
-	IPAddress string
-	Port      int
-}
-
-var NodeConfigs = []NodeConfig{
-	{NodeID: 1, IPAddress: "127.0.0.1", Port: 0},
-	{NodeID: 2, IPAddress: "127.0.0.1", Port: 0},
-	{NodeID: 3, IPAddress: "127.0.0.1", Port: 0},
-}
+var IPAddress = "127.0.0.1"
+var NodeCount = 3
