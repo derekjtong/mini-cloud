878bc09df8e5c6213fca5407ac867f61591782b4
Hcy999 <m568912049@gmail.com>
2023-11-19 22:01:56 -0500

    update paxos scenarios

--- main.go
+++ main.go
@@ -12,8 +12,12 @@ import (
 	"github.com/derekjtong/paxos/node"
 	"github.com/derekjtong/paxos/utils"
 
-	myRPC "github.com/derekjtong/paxos/rpc"
+	MyRPC "github.com/derekjtong/paxos/rpc"
 )
+
+type NodeIPs struct {
+	IPs []string
+}
 
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "client" {
@@ -21,70 +25,81 @@ func main() {
 	} else {
 		startServer()
 	}
+
 }
 
 func startClient() {
-	fmt.Print("Starting Client!\n\nNode IP address: (defaulting to 127.0.0.1)\n")
-
-	var ipAddress string = "127.0.0.1"
+	fmt.Print("Starting Client!\nNode IP address: (defaulting to 127.0.0.1)\n")
+	var IPAddress string = "127.0.0.1"
 	// fmt.Scanln(&IPAddress)
-
 	fmt.Print("Node port number: ")
-	var port int
-	fmt.Scanln(&port)
-
-	fmt.Printf("Pinging %s:%d\n", ipAddress, port)
-	client, err := rpc.Dial("tcp", fmt.Sprintf("%s:%d", ipAddress, port))
+	var Port int
+	fmt.Scanln(&Port)
+	fmt.Printf("Connecting to %s:%d...\n", IPAddress, Port)
+	client, err := rpc.Dial("tcp", fmt.Sprintf("%s:%d", IPAddress, Port))
 	if err != nil {
-		fmt.Printf("Error dialing RPC server, please confirm port number.\n            (%v)\n", err)
+		fmt.Printf("Error dialing RPC server:%v\n", err)
 		os.Exit(1)
 	}
-
 	defer client.Close()
-
-	var request myRPC.PingRequest
-	var response myRPC.PingResponse
+	var request MyRPC.PingRequest
+	var response MyRPC.PingResponse
 	if err := client.Call("RPCServer.Ping", &request, &response); err != nil {
 		fmt.Printf("Error calling RPC method: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Printf("%+v. Connected!\n", response.Message)
+	fmt.Printf("Reply: %+v\n", response)
 }
 
 func startServer() {
 	fmt.Printf("Starting server! Hint: to start client, 'go run main.go client'.\n\n")
 	var wg sync.WaitGroup
-	var nodeNeighbors []string
-	for nodeID := 1; nodeID <= utils.NodeCount; nodeID++ {
+	var rpcWG sync.WaitGroup
+
+	for _, config := range utils.NodeConfigs {
+		// Increment the main wait group for each node
 		wg.Add(1)
+
+		// Increment the RPC wait group for each node
+		rpcWG.Add(1)
+
+		// Dynamically find an available port
 		port, err := findAvailablePort()
 		if err != nil {
 			fmt.Printf("Error finding available port: %v\n", err)
 			return
 		}
-		addr := fmt.Sprintf("%s:%d", utils.IPAddress, port)
-		nodeNeighbors = append(nodeNeighbors, addr)
-		go func(ipAddress string, nodeID int, port int, wg *sync.WaitGroup) {
+
+		// Start Goroutine for node
+		go func(config utils.NodeConfig, port int, wg *sync.WaitGroup, rpcWG *sync.WaitGroup) {
 			defer wg.Done()
-			fmt.Printf("[Node %d]: Starting on %s\n", nodeID, addr)
-			node := node.NewNode(nodeID, addr)
-			node.Start()
-		}(utils.IPAddress, nodeID, port, &wg)
+
+			fmt.Printf("[Node%d]: Starting on %s:%d\n", config.NodeID, config.IPAddress, port)
+			node := node.NewNode(config.NodeID, config.IPAddress, port)
+
+			// Start the node
+			node.Start(rpcWG)
+
+		}(config, port, &wg, &rpcWG)
 	}
-	// Send NodeNeighbors to every node
+
+	// Wait for all nodes to finish starting
 	wg.Wait()
+
+	// Keep the main function running to keep the servers active
 	select {}
 }
 
-// Find an available port
 func findAvailablePort() (int, error) {
+	// Find a free port
 	listener, err := net.Listen("tcp", ":0")
 	if err != nil {
 		return 0, err
 	}
 	defer listener.Close()
 
+	// Get the allocated port
 	address := listener.Addr().String()
 	_, portString, err := net.SplitHostPort(address)
 	if err != nil {
--- paxos/paxos.go
+++ paxos/paxos.go
@@ -21,6 +21,17 @@ func (n *Node) Prepare(proposal string) (string, bool) {
 	return n.Proposal, false
 }
 
+func (n *Node) Propose(proposal string) {
+	// Simulate network delay
+	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	prop, ok := n.Prepare(proposal)
+	if ok {
+		fmt.Printf("Node %d prepared proposal: %s\n", n.ID, prop)
+	} else {
+		fmt.Printf("Node %d already accepted proposal: %s\n", n.ID, n.Proposal)
+	}
+}
+
 func (n *Node) Accept(proposal string) bool {
 	if !n.Accepted {
 		n.Proposal = proposal
@@ -83,4 +94,65 @@ func RunPaxos(nodes []*Node, proposal string) {
 			fmt.Printf("Node %d has accepted proposal: %s\n", node.ID, node.Proposal)
 		}
 	}
+}
+
+func SingleProposerScenario() {
+	// Scenario with a single proposer
+	nodes := []*Node{{ID: 1}, {ID: 2}, {ID: 3}}
+	proposal := "Single Proposer Scenario"
+	RunPaxos(nodes, proposal)
+}
+
+func TwoProposersScenario() {
+	// Scenario with two proposers (clients A and B)
+	nodes := []*Node{{ID: 1}, {ID: 2}}
+	proposal := "Two Proposers Scenario"
+	RunPaxos(nodes, proposal)
+}
+
+func TwoProposersAWinsScenario() {
+	// Scenario where A wins
+	nodeA := &Node{ID: 1}
+	nodeB := &Node{ID: 2}
+
+	// Simulate previous value already chosen by another node
+	nodeA.Accept("Previous value")
+
+	// Introduce random delays to simulate network effects
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		nodeA.Propose("New Value from A")
+	}()
+
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		nodeB.Propose("New Value from B")
+	}()
+
+	time.Sleep(500 * time.Millisecond) // Allow time for proposal and acceptance
+
+	nodes := []*Node{nodeA, nodeB}
+	RunPaxos(nodes, "New Value from A or B")
+}
+
+func TwoProposersBWinsScenario() {
+	// Scenario where B wins
+	nodeA := &Node{ID: 1}
+	nodeB := &Node{ID: 2}
+
+	// Introduce random delays to simulate network effects
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		nodeA.Propose("New Value from A")
+	}()
+
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		nodeB.Propose("New Value from B")
+	}()
+
+	time.Sleep(500 * time.Millisecond) // Allow time for proposal and acceptance
+
+	nodes := []*Node{nodeA, nodeB}
+	RunPaxos(nodes, "New Value from A or B")
 }
