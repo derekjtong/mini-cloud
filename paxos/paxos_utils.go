54b46f62e2f1d833439fd387aec07e47664ebf6e
Derek <109226053+derekjtong@users.noreply.github.com>
2023-11-21 22:23:42 -0500

    Added paxos

--- paxos/paxos_utils.go
+++ paxos/paxos_utils.go
@@ -2,11 +2,13 @@ package paxos
 
 // Prepare phase request
 type PrepareRequest struct {
+	Id       int
 	Proposal int
 }
 
 // Prepare phase response
 type PrepareResponse struct {
+	Id            int
 	OK            bool
 	Proposal      int
 	AcceptedValue string
@@ -14,12 +16,14 @@ type PrepareResponse struct {
 
 // Accept phase request
 type AcceptRequest struct {
+	Id       int
 	Proposal int
 	Value    string
 }
 
 // Accept phase response
 type AcceptResponse struct {
+	Id       int
 	OK       bool
 	Proposal int
 }
